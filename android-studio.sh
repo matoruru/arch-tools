66116d6c63290a836b4f4cf7d594a107f23a8b8b
matoruru <matoruru@shgt.jp>
2019-03-02 02:32:47 +0900

    for android-studio

---
+++ android-studio.sh
@@ -0,0 +1,8 @@
+sudo pacman -S jdk-openjdk
+yay -S android-platform android-sdk android-sdk-platform-tools android-sdk-build-tools
+
+sudo groupadd sdkusers
+sudo gpasswd -a matoruru sdkusers
+chown -R :sdkusers /opt/android-sdk/
+sudo chown -R :sdkusers /opt/android-sdk/
+sudo chmod -R g+w /opt/android-sdk/
