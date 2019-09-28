93a027b239eea17db1af0d73be465733c6747bb8
matoruru <40382980+matoruru@users.noreply.github.com>
2019-09-28 16:58:16 +0900

    Update whole.sh

--- steps/whole.sh
+++ steps/whole.sh
@@ -7,6 +7,12 @@ ROOTPASSWORD=$4
 USERNAME=$5
 USERPASSWORD=$6
 
+# check if 6 args exists
+[ -z "$6" ] && { echo "You need to input 6 arguments!"; exit; }
+
+# check a connection to internet
+ping www.google.com -i 0.2 -c 5 || { echo "Connection is not established..."; exit; }
+
 bash phase1.sh $PARTSIZE
 arch-chroot /mnt bash -c "bash phase2.sh $HOSTNAME $INTERFACE $ROOTPASSWORD $USERNAME $USERPASSWORD"
 
