68cab0a32d8906afbe92546ab86ceeba6028b1bc
matoruru <matoruru@shgt.jp>
2019-09-29 17:53:21 +0900

    modify

--- steps/whole.sh
+++ steps/whole.sh
@@ -6,13 +6,24 @@ INTERFACE=$3
 ROOTPASSWORD=$4
 USERNAME=$5
 USERPASSWORD=$6
+ROOTSCRIPT=$7
+USERSCRIPT=$8
 
-# check if 6 args exists
+# check if 8 args exists
 [ -z "$6" ] && {
-  echo "You need to input 6 arguments!";
+  echo "You need to input 8 arguments!";
   echo "
-  EXAMPLE:
-   $ bash <(curl -s https://raw.githubusercontent.com/matoruru/arch-tools/master/steps/whole.sh) +60G hostname wlp2s0 rootpasswd myusername myuserpasswd
+  EXAMPLE 1:
+   $ bash <(curl -s https://raw.githubusercontent.com/matoruru/arch-tools/master/steps/whole.sh) +60G hostname wlp2s0 rootpasswd myusername myuserpasswd \"\$(curl -s https://.../...sh)\" \"\$(curl -s https://.../...sh)\"
+
+  EXAMPLE 2:
+   $ bash <(curl -s https://raw.githubusercontent.com/matoruru/arch-tools/master/steps/whole.sh) +60G hostname wlp2s0 rootpasswd myusername myuserpasswd \"echo \\\"I am root.\\\";\" \"echo \\\"I am\\\"; echo \\\"the new user.\\\";\"
+
+  EXAMPLE 3:
+   $ bash <(curl -s https://raw.githubusercontent.com/matoruru/arch-tools/master/steps/whole.sh) +60G hostname wlp2s0 rootpasswd myusername myuserpasswd \"\$(cat file.sh)\" \"\"
+
+  EXAMPLE 2:
+   $ bash <(curl -s https://raw.githubusercontent.com/matoruru/arch-tools/master/steps/whole.sh) +60G hostname wlp2s0 rootpasswd myusername myuserpasswd \"\" \"\"
   "
   exit;
 }
@@ -24,5 +35,11 @@ BASEURL="https://raw.githubusercontent.com/matoruru/arch-tools/master/steps"
 
 bash <(curl -s $BASEURL/phase1.sh) $PARTSIZE
 arch-chroot /mnt bash -c "bash <(curl -s $BASEURL/phase2.sh) $HOSTNAME $INTERFACE $ROOTPASSWORD $USERNAME $USERPASSWORD"
+
+# Execute a script($ROOTSCRIPT) as root after base install, start at ~/
+arch-chroot /mnt bash -c "cd; $ROOTSCRIPT"
+
+# Execute a script($USERSCRIPT) as a user after base install, start at ~/
+arch-chroot /mnt bash -c "sudo -u $USERNAME bash -c \"cd; $USERSCRIPT\""
 
 reboot
