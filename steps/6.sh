e0bd65dfc8c94102c83587955f9144c5bada7255
matoruru <40382980+matoruru@users.noreply.github.com>
2019-12-22 11:42:44 +0900

    Update 6.sh

--- steps/6.sh
+++ steps/6.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pacstrap /mnt base base-devel grub dosfstools efibootmgr intel-ucode netctl wpa_supplicant openssh git dialog linux
+pacstrap /mnt base base-devel grub dosfstools efibootmgr intel-ucode netctl wpa_supplicant openssh git dialog linux udev linux-firmware
