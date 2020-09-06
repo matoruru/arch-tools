8257553df07f4df8956701a40fb44ab0a553efb4
matoruru <matoruru@shgt.jp>
2020-09-06 15:53:34 +0900

    Refactor

--- screenshot.sh
+++ screenshot.sh
@@ -3,36 +3,13 @@
 # Usage
 # screenshot.sh  displaying time  zoom ratio  [--focused]
 
-if [[ ! -d $HOME/Pictures/Screenshots/ ]]; then
-   mkdir -p ~/Pictures/Screenshots
-fi
-cd          ~/Pictures/Screenshots
-
-tmpname=_____scReeNshoT-tmP.png
-if [[ -f $tmpname ]]; then
-   exit
-fi
-
-newname=$( scrot -q 100 $3 '%Y-%m-%d-%T-screenshot.png' -e 'echo $f' )
-ln -s $newname $tmpname
-
-# To make $disptime from 0 to 5
-if   [[ $(echo "$1 <= 0" | bc) -eq 1 ]]; then
-   echo "redefine $1 as 0.1..."
-   disptime=0.1
-elif [[ $(echo "$1 >  5" | bc) -eq 1 ]]; then
-   echo "redefine $1 as 5..."
-   disptime=5
-else
-   disptime=$1
-fi
-
+disptime=$1
 zoomratio=$2
+focused=$3
 
-timeout $disptime feh --fullscreen --zoom $zoomratio $tmpname
+mkdir -p ~/Pictures/Screenshots
+cd       ~/Pictures/Screenshots
 
-# if $disptime is lower than 1, after sleep 1 - $disptime, rm
-if [[ $(echo "$disptime < 1" | bc) -eq 1 ]]; then
-   sleep $(echo "1 - $disptime" | bc)
-fi
-rm $tmpname
+newname=$( scrot -q 100 $focused '%Y-%m-%d-%T-screenshot.png' -e 'echo $f' )
+
+timeout $disptime feh --fullscreen --zoom $zoomratio $newname
